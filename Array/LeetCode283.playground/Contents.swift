a6fb9d5b9ae2409891a289da6ddaff1c1f984b32
Jackie <>
2023-07-26 18:22:20 +1000

    LeetCode283

---
+++ Array/LeetCode283.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+// LeetCode 283 - Move Zeroes
+
+// Hint: Two pointers
+
+func moveZeroes(_ nums: inout [Int]) {
+    var j = 0
+    for i in 0..<nums.count where nums[i] != 0 {
+        nums[j] = nums[i]
+        j += 1
+    }
+    for r in j..<nums.count {
+        nums[r] = 0
+    }
+}
